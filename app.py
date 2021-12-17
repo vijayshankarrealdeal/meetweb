1af8325c82ded8b23d6d710a82f2c6ce94114d89
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-12-17 15:44:16 +0530

    cc

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import  requests
+from werkzeug.exceptions import HTTPException
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/get-message', methods=['GET'])
+def getMessage():
+    data = {'message': 'Hello World!'}
+    return jsonify(data)
+
+
+@app.route('/api/getmovies',methods=['GET'])
+def get_movieList():
+    api_key = '38f5b3c12b04920fbe5fd093187951af'
+    url = 'https://api.themoviedb.org/3/trending/all/day?api_key='+api_key
+    data = requests.get(url)
+    return jsonify({'data':data.text})
+
+app.config["DEBUG"] = True
