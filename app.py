28ab97db068ac817ef938a506cb8e6c0c45fadb8
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-12-18 23:06:43 +0530

    dd

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import  requests
 from flask_cors import CORS
 import pandas as pd
 import os
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
 
 from db import create_table, insert_data, read_data
 app = Flask(__name__)
@@ -151,11 +153,36 @@ def get(type_shop):
         x = [df.T.to_dict()[i] for i in df.T.to_dict()]
     return jsonify({"data":x})
 ############################################### GEt status########################
-# @app.route('/api/getflightstatus',methods=["GET"])
-# def get_board():
-#     url = 'https://mini-bell-bottom.herokuapp.com/getflightstatus/'
-#     data = requests.get(url)
-#     print(data)
-#     return data.json()
+@app.route('/api/getflightstatus',methods=["GET"])
+def get_board():
+    options = webdriver.ChromeOptions()
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(
+        executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+    driver.maximize_window()
+    driver.get('https://www.bangaloreairport.com/kempegowda-departures')
+    items = driver.find_elements_by_xpath('.//div[@class = "flight-row"]')
+    data = []
+    for item in items:
+        try:
+            k = {}
+            k['departure'] = item.find_element_by_xpath('.//div[1]').text
+            k['time'] = item.find_element_by_xpath('.//div[2]//div[1]').text
+            k['flight'] = item.find_element_by_xpath('.//div[2]//div[2]').text
+            k['airline'] = item.find_element_by_xpath('.//div[2]//div[3]').text
+            k['info_url'] = item.find_element_by_xpath('.//div[2]').find_element_by_tag_name('a').get_attribute('href')
+            k['status'] = item.find_element_by_xpath('.//div[contains(@class ,"flight-col flight-col__status")]').text
+            data.append(k)
+        except:
+            pass
+    df = pd.DataFrame(data)
+    driver.close()
+    data = [df.T.to_dict()[i] for i in df.T.to_dict()]
+    return jsonify({"data":data})
+
 
 app.config["DEBUG"] = True
