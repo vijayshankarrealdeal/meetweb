4e58e180d971cfd2b75a17477d35d622acb49400
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-12-27 17:25:08 +0530

    jjjhfhhgfhgg

--- app.py
+++ app.py
@@ -60,7 +60,14 @@ def register(email,password1):
 
 @app.route('/api/userforgotpass/<string:uid><string:currentpass>/<string:newpassword>')
 def updatePassword(uid,currentpass,newpas):
-        return jsonify({"data":"e"})
+    cursor.execute("""SELECT * from "USERS" WHERE uid = %s""",(uid,),)
+    rows = cursor.fetchall()
+    oldpass = [row[2] for row in rows]
+    if oldpass == currentpass:
+        cursor.execute("""UPDATE "USERS" SET password = %s WHERE uid = %s""",(newpas, uid))
+        return jsonify({"status":"done"}) 
+    else:  
+        return jsonify({"error":"current_password is wrong"})
 
 
 
